500df0cba7320c5b4761fffa9e7b27f764362c52
Egor Gerdiy <egor.gerdiy@gmail.com>
2025-06-10 10:36:48 +0300

    Initial Commit

---
+++ Finance ShMR/Finance_ShMRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_ShMRApp.swift
+//  Finance ShMR
+//
+//  Created by Egor Herdziy on 10.06.25.
+//
+
+import SwiftUI
+
+@main
+struct Finance_ShMRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
